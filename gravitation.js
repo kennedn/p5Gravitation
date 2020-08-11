92512d031b096eebfb6afa94d54076fed0351ce4
kennedn <kennedn@msn.com>
2020-08-12 00:43:32 +0100

    Themed for website, removed colour changing, added slider

--- gravitation.js
+++ gravitation.js
@@ -1,102 +1,96 @@
 let maxDistance;
 let spacer = 30;
-let grav_max = 2000;
-let mx = 1;
-let my = 1;
+let targetMouseX = 1;
+let targetMouseY = 1;
 let easing = 0.5;
 let scale = 0.5;
 
+let strokeColor;
 let currentColor;
-let targetColor;
-let lerpAmount = 0;
-let colorRate = 1.1;
-let finished_lerp;
-let colors = ["#1B2021", "#112d22", "#450156", "#4c0c0c", "#0f1e4f"];
-let mouse_multiplier = 0;
-let target_multiplier = 0;
-let mouse_lerp = 0;
-let mouse_power = 2
+let mouseMultiplier = 0;
+let targetMultiplier = 0;
+let mouseLerp = 0;
+let mousePower = 2;
+
+let multiplierSlider;
 let font;
 let cnv; 
+
+// Preload font
 function preload() {
     font = loadFont('font/Ubuntu-B.ttf');
 }
 
 function setup() {
-   cnv = createCanvas(displayWidth, displayHeight);
-   cnv.mouseWheel(changeMultiplier);
-   maxDistance = dist(0, 0, displayWidth, displayHeight);
-
-   currentColor = color(colors[0]);
-   targetColor = currentColor;
-   lerp_finished = true;
-   fill(255);
-   fullscreen();
-   textFont(font);
-   //textAlign(CENTER);
+  // Set canvas size
+  cnv = createCanvas(1080, 720);
+  // Attach listener to canvas for mouseWheel events
+  cnv.mouseWheel(changeMultiplier);
+  // Calc the largest vector possible based on canvas size
+  maxDistance = dist(0, 0, width, height);
+  multiplierSlider = createSlider(-1.5, 1.5, 0, 0.05);
+  multiplierSlider.position(20, 10);
+  multiplierSlider.style('width', width - 40);
+  currentColor = color("#6e04a9");
+  fill(255);
+  strokeColor = color(255);
+  fullscreen();
+  textFont(font);
 }
 
-function windowResized() {
-    resizeCanvas(displayWidth, displayHeight);
-}
-
-
-function mouseClicked() {
-  let tempC = colors.pop();
-  reverse(colors);
-  colors.push(tempC);
-  reverse(colors);
-  recolor(colors[0]); 
-}
-
+// MouseWheel callback, increase/decrease targetMultipler based on scroll direction
 function changeMultiplier(event) {
-  mouse_lerp = 0.01;
-  if (event.deltaY > 0 && target_multiplier < 1.5)
-    target_multiplier += 0.1;
-  else if (event.deltaY < 0 && target_multiplier > -1.5)
-    target_multiplier -= 0.1;
+  mouseLerp = 0.01;
+  if (event.deltaY > 0 && targetMultiplier < 1.5)
+    targetMultiplier += 0.1;
+  else if (event.deltaY < 0 && targetMultiplier > -1.5)
+    targetMultiplier -= 0.1;
+  // Update slider with current multiplier
+  multiplierSlider.value(-targetMultiplier);
 }
 
-function recolor(c) {
-  lerpAmount = 0.01;
-  targetColor = color(c); 
-}
-
+// Main draw function
 function draw() {
   background(currentColor);
 
-
-  if (lerpAmount < 1) {
-    currentColor = lerpColor(currentColor, targetColor, lerpAmount);
-    lerpAmount = (lerpAmount * colorRate) * (1 + lerpAmount); 
+  // trigger a multiplier lerp if slider is modified
+  if (targetMultiplier != -multiplierSlider.value()) {
+    targetMultiplier = -multiplierSlider.value();
+    mouseLerp = 0.01;
   }
-  else
-    lerp_finished = true;
-
-  if (mouse_lerp < 1) {
-    mouse_multiplier = lerp(mouse_multiplier, target_multiplier, mouse_lerp);
-    mouse_lerp = (mouse_lerp * 1.3); //* (1 + mouse_lerp);
+  // Lerp towards targetMultiplier
+  if (mouseLerp < 1) {
+    mouseMultiplier = lerp(mouseMultiplier, targetMultiplier, mouseLerp);
+    mouseLerp = (mouseLerp * 1.3); //* (1 + mouseLerp);
   }
-  if (abs(mouseX - mx) > 0.1)
-    mx = mx + (mouseX - mx) * easing;
 
-  if (abs(mouseY - my) > 0.1)
-    my = my + (mouseY - my) * easing;
+
+  // Ease towards tracked mouse location, speed decays based on distance
+  if (abs(mouseX - targetMouseX) > 0.1)
+    targetMouseX = targetMouseX + (mouseX - targetMouseX) * easing;
+  if (abs(mouseY - targetMouseY) > 0.1)
+    targetMouseY = targetMouseY + (mouseY - targetMouseY) * easing;
+
+  // Draw a 2d array of points 
   for (let x = 0; x < width; x += spacer) {
     for (let y = 0; y < height; y += spacer) {
-      let mouseVector = createVector(mx - x, my - y).normalize();
-      let mouseDistance = dist(x, y, mx, my) / spacer;
-      let gravity_scaler =  -mouseDistance * mouse_multiplier + pow(mouseDistance, mouse_power) / (spacer / 2) ;
-      let t_x = x - mouseVector.x * 100 * gravity_scaler;
-      let t_y = y - mouseVector.y * 100 * gravity_scaler;
-      if ((t_x >= 0 && t_x <= width) && (t_y >= 0 && t_y <= height)) {
-        let strokeAlpha = 255;
-        let strokeColor = color(255,255,255, strokeAlpha);
+      // Calculate a normalized direction between mouse pointer and current point[x,y]
+      let mouseVector = createVector(targetMouseX - x, targetMouseY - y).normalize();
+      // Calculate the real distance between mouse pointer and point[x,y]
+      let mouseDistance = dist(x, y, targetMouseX, targetMouseY) / spacer;
+      // Calculate a scaler roughly based on the square law (anti-grav) = -distance * modifier + distance^2 / constant
+      let gravityScaler =  -mouseDistance * mouseMultiplier + pow(mouseDistance, mousePower) / (spacer / 2) ;
+
+      // Calculate a modified x,y, moving the point away from mouse using the gravityScaler 
+      let tempX = x - mouseVector.x * 100 * gravityScaler;
+      let tempY = y - mouseVector.y * 100 * gravityScaler;
+      // Only draw a point if its within canvas bounds
+      if ((tempX >= 0 && tempX <= width) && (tempY >= 0 && tempY <= height)) {
         stroke(strokeColor);
-        strokeWeight(constrain(4/mouseDistance + 2, 2, 4));
-        if (strokeAlpha > 1) {
-          point(t_x, t_y);
-          }
+        // Modify size of point based on mouseDistance
+        strokeWeight(constrain(8/mouseDistance + 4, 4, 8));
+        // Draw point
+        point(tempX, tempY);
       }
     }
   } 
